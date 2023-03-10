8e78601704ce54d18f4b2f57f8b703e5e79546c3
kbc20f11 <82624729+kbc20f11@users.noreply.github.com>
2023-03-10 17:03:50 +0900

    delete some shit

--- common/jumbotron.php
+++
@@ -1,6 +0,0 @@
-<div class="p-5 mb-4 bg-light rounded-3">
-      <div class="container-fluid py-5">
-        <h1 class="display-5 fw-bold"><?= $title_jumbo ?></h1>
-        <p class="col-md-8 fs-4"><?= $desc_jumbo ?></p>
-      </div>
-    </div>
